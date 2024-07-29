e7f8d38ec5a2f4ddbde4a6483bd44078411fe69e
Antonio Punzo <antonio.punzo5@unibo.it>
2024-07-29 11:08:48 +0200

    wip

--- src/Heuristic.cpp
+++ src/Heuristic.cpp
@@ -13,13 +13,13 @@
 
 
 
-Result run(Instance& inst, Args const& args, GRBEnv& env, int timelimit) {
+Result run(Instance& inst, Args const& args, GRBEnv& env, RandomGenerator& rand_gen, int timelimit) {
 
 	Timer timer{};
 	timer.start("total");
 
-	Preprocessing prepro{};
-	prepro.run(inst);
+	Preprocessing prepro(inst);
+	prepro.run(inst,rand_gen);
 
 // -------------------------------------------------------- //
 
@@ -31,7 +31,7 @@ Result run(Instance& inst, Args const& args, GRBEnv& env, int timelimit) {
 		if(!carp_inst.link_to_visit.empty()){
 
 		auto solver = RouteSolver{};
-		auto routes = solver.solve_routes(inst, k, carp_inst, static_cast<int>(timelimit * 0.1), args.iterlimit);
+		auto routes = solver.solve_routes(inst, k, carp_inst, static_cast<int>(args.timelimit*0.1), args.iterlimit);
 		all_routes.insert(all_routes.end(), routes.begin(), routes.end());
 
 			for(auto const& route: routes){
@@ -41,9 +41,8 @@ Result run(Instance& inst, Args const& args, GRBEnv& env, int timelimit) {
 	}
 	// print_routes(inst, "data/sol/"+inst.name+"_sol.txt", all_routes);
 	int nroutes = (int) all_routes.size();
-
-	fmt::print("VIDAL END!\n");
-
+	timer.stop("vidal");
+	std::cout << "VIDAL END!" << std::endl;
 // -------------------------------------------------------- //
 
 	RTModel rt_model{env, inst, args, all_routes};	
@@ -55,8 +54,8 @@ Result run(Instance& inst, Args const& args, GRBEnv& env, int timelimit) {
 
 // -------------------------------------------------------- //
 
-	int timelimit_ls = args.timelimit - static_cast<int>(timelimit * 0.9);
-	local_search(env, inst, args, best, rt_res, timelimit_ls, 20);
+	int timelimit_ls = args.timelimit - timer.duration("vidal");
+	local_search(env, inst, args, best, rt_res, timelimit_ls, 3);
 
 	timer.stop("total");
 
@@ -70,8 +69,8 @@ Result run(Instance& inst, Args const& args, GRBEnv& env, int timelimit) {
 	res.vidal_time = RouteSolver::call_time;
 	res.rt_time = RTModel::call_time;
 	res.ls_time = local_search_time;
-	res.gurobi_time = RTModel::grb_time;
 
+	res.gurobi_time = RTModel::grb_time;
 	res.total_time = timer.duration("total");
 
 	res.time_ls = best.time;
@@ -86,33 +85,14 @@ Result run(Instance& inst, Args const& args, GRBEnv& env, int timelimit) {
 	return res;
 }
 
-void update_total_res(Result& total_res, Result& res) {
-	
-	total_res.vidal_time = RouteSolver::call_time;
-	total_res.rt_time = RTModel::call_time;
-	total_res.ls_time = local_search_time;
-	total_res.gurobi_time = RTModel::grb_time;
-
-	if (res.ls_obj < total_res.ls_obj) {
-		total_res.ls_obj = res.ls_obj;
-		total_res.vidal_obj = res.vidal_obj;
-		total_res.rt_obj = res.rt_obj;
-		total_res.rt_obj = res.rt_obj;
-		total_res.time_ls = res.time_ls;
-		total_res.iter_ls = res.iter_ls;
-		total_res.best_iter_ls = res.best_iter_ls;
-		total_res.best_time_ls = res.best_time_ls;
-		total_res.nroutes = res.nroutes;
-		total_res.lb = res.lb;
-	}
-}
-
 
 Result heur(Instance& inst, Args const& args) {
 
 // -------------------------------------------------------- //
 	Timer timer{};
 	timer.start("total");
+
+	RandomGenerator rand_gen{};
 
 	GRBEnv env{};
 
@@ -122,21 +102,15 @@ Result heur(Instance& inst, Args const& args) {
 		env.set(GRB_IntParam_OutputFlag, 1);
 	#endif
 
-	Result total_res;
-
 	double timelimit = args.timelimit;
-	auto res = run(inst, args, env, timelimit);
-	update_total_res(total_res, res);
-
+	auto res = run(inst, args, env, rand_gen, timelimit);
 	if (std::abs(res.total_time - timelimit) > 10) {
 		timelimit -= res.total_time;
 		fmt::print("restart with residual timelimit {}\n", timelimit);
-		res = run(inst, args, env, timelimit);
-		update_total_res(total_res, res);
+		res = run(inst, args, env, rand_gen, timelimit);
 	}
 
 	timer.stop("total");
-	total_res.total_time = timer.duration("total");
 
 // -------------------------------------------------------- //
 
